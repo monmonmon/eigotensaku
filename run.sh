b4fddfeb85f4bd3a6acfcfb4a495a2958c3d4c0f
monmonmon <ymdsmn@gmail.com>
2014-02-17 16:31:21 +0900

    :P

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ruby main.rb \
-    "jiOCarvP4nCmnwZD5OqLjw" \
-    "nJve9QimDseNydejrC6KRkmzXa4XgdwSCXBFWH8eM" \
-    "2301630019-9aI8ToQDOPABtEdvoq4nHGUqODj7sBArE2a55xY" \
-    "vuK7vowoe8qJ3YUjZmKmCUT5ZepqtFjmDdFrB3DRpSihh"
